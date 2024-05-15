ca967b262f28b7c7af8d1d2e2c9850847ec8bd0d
thomasuriel1 <tlauravargas@gmail.com>
2024-05-15 11:13:58 -0300

    Hice la lista de lenguajes

--- cuervana3/__init__.py
+++ cuervana3/__init__.py
@@ -20,3 +20,15 @@ def actores():
 
     return paginaActores
 
+@app.route('/lenguaje')
+def lenguaje():
+    lenguaje = """
+        SELECT name FROM language
+        ORDER BY name ASC
+    """
+    con = db.get_db()
+    res = con.execute(lenguaje)
+    lista_lenguaje = res.fetchall()
+    paginaLenguaje = render_template('lenguaje.html', lenguaje=lista_lenguaje )
+
+    return paginaLenguaje
