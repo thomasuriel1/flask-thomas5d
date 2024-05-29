745fe64ea6efe09013ac27c396f388f93f2c9f0a
thomasuriel <tlauravargas@gmail.com>
2024-05-29 11:19:16 -0300

    hice el detalle de actor

--- cuervana3/actores.py
+++ cuervana3/actores.py
@@ -3,16 +3,39 @@ from . import db
 
 bp = Blueprint('actor', __name__, url_prefix = "/actores")
 
-
-@bp.route('/')
+@bp.route("/")
 def actores():
     actoresNyA = """
-        SELECT first_name, last_name FROM actor
+        SELECT first_name, last_name, actor_id FROM actor
         ORDER BY first_name, last_name ASC
     """
     con = db.get_db()
     res = con.execute(actoresNyA)
-    lista_actor = res.fetchall()
-    paginaActores = render_template('actores.html', actores=lista_actor )
+    actores = res.fetchall()
+    pagina = render_template('actores.html', actores=actores)
+    return pagina
+
+
+@bp.route('/<int:id>')
+def detalle(id):
+    actoresNyA = """
+        SELECT first_name, last_name FROM actor
+        WHERE actor_id = ?
+    """
+    con = db.get_db()
+    res = con.execute(actoresNyA,(id,))
+    actor = res.fetchone()
+
+
+    actoresNyA2 = """
+        SELECT title AS titulo, f.film_id FROM film f
+        JOIN film_actor fi ON f.film_id = fi.film_id
+        WHERE fi.actor_id = ?
+    """
+    res = con.execute(actoresNyA2,(id,))
+    lista_peliculas = res.fetchall()
+    paginaActores = render_template('detalle_actor.html',
+                                    actor=actor, 
+                                    peliculas=lista_peliculas )
 
     return paginaActores
